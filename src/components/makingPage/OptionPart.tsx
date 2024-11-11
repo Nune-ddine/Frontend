08a2f6a2c808aa4870330a8bf170534e5b26a4f7
suzzang2 <subin030102@naver.com>
2024-11-11 23:29:28 +0900

    FEAT: FeatureBar 토글하면 뜨기

--- src/components/makingPage/OptionPart.tsx
+++ src/components/makingPage/OptionPart.tsx
@@ -1,5 +1,5 @@
 // src/components/OptionPart.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import WheelPickerTest from './optionComponents/WheelPickerTest';
 import FeatureBar from './optionComponents/FeatureBar';
@@ -9,12 +9,29 @@ interface OptionPartProps {
 }
 
 const OptionPart: React.FC<OptionPartProps> = ({ onColorChange }) => {
+  const [selectedFeature, setSelectedFeature] = useState<string | null>(null);
+
+  const renderFeatureContent = () => {
+    switch (selectedFeature) {
+      case "형태":
+        return <div>형태 설정 옵션</div>;
+      case "색":
+        return <WheelPickerTest onColorChange={onColorChange} />;
+      case "얼굴":
+        return <div>얼굴 설정 옵션</div>;
+      case "옷":
+        return <div>옷 설정 옵션</div>;
+      case "DIY":
+        return <div>DIY 설정 옵션</div>;
+      default:
+        return null;
+    }
+  };
+
   return (
     <Wrapper>
-      <FeatureBar />
-      <SecondFeature>
-         <WheelPickerTest onColorChange={onColorChange} />
-      </SecondFeature>
+      <FeatureBar onFeatureClick={(feature) => setSelectedFeature(feature)} />
+      <SecondFeature>{renderFeatureContent()}</SecondFeature>
     </Wrapper>
   );
 };
@@ -26,17 +43,16 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-
-   width: 100%;
-   height: 55%;
-   border: 2px solid black;
+  width: 100%;
+  height: 55%;
+  border: 2px solid black;
 `;
 
 const SecondFeature = styled.div`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   width: 100%;
-   height: 100%;
-   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
 `;
--- src/components/makingPage/optionComponents/FeatureBar.tsx
+++ src/components/makingPage/optionComponents/FeatureBar.tsx
@@ -1,41 +1,44 @@
-import styled from "styled-components"
+// src/components/optionComponents/FeatureBar.tsx
+import styled from 'styled-components';
 
-const FeatureBar = () => {
-   return (
-      <Wrapper>
-         <Feature>형태</Feature>
-         <Feature>색</Feature>
-         <Feature>얼굴</Feature>
-         <Feature>옷</Feature>
-         <Feature>DIY</Feature>
-      </Wrapper>
-   )
+interface FeatureBarProps {
+  onFeatureClick: (feature: string) => void;
 }
 
-export default FeatureBar
+const FeatureBar: React.FC<FeatureBarProps> = ({ onFeatureClick }) => {
+  return (
+    <Wrapper>
+      <Feature onClick={() => onFeatureClick("형태")}>형태</Feature>
+      <Feature onClick={() => onFeatureClick("색")}>색</Feature>
+      <Feature onClick={() => onFeatureClick("얼굴")}>얼굴</Feature>
+      <Feature onClick={() => onFeatureClick("옷")}>옷</Feature>
+      <Feature onClick={() => onFeatureClick("DIY")}>DIY</Feature>
+    </Wrapper>
+  );
+};
+
+export default FeatureBar;
 
 const Wrapper = styled.div`
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   width: 100%;
-   height: 15%;
-   border: 2px solid black;
-
-   padding: 10px;
-   
-   box-sizing: border-box;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 15%;
+  border: 2px solid black;
+  padding: 10px;
+  box-sizing: border-box;
+`;
 
 const Feature = styled.button`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   width: 15%;
-   height: 100%;
-   border-radius: 5px;
-   background-color: #c7c7c7;
-   font-size: 0.8rem;
-   text-align: center;
-   cursor: pointer;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 15%;
+  height: 100%;
+  border-radius: 5px;
+  background-color: #c7c7c7;
+  font-size: 0.8rem;
+  text-align: center;
+  cursor: pointer;
+`;
