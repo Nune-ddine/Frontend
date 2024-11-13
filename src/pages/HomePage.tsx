b87afc985552afe95393139c963790cccd28c282
jungsunbeen <tjqls041103@gmail.com>
2024-11-13 17:17:09 +0900

    FEAT : Locating ADD : fonts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -17,7 +17,7 @@ const HomePage = () => {
 
 export default HomePage
 
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
   height : 100%;
   display : flex;
   flex-direction: column;
--- src/pages/LocatingPage.tsx
+++ src/pages/LocatingPage.tsx
@@ -1,9 +1,44 @@
-const locatingPage = () => {
-  return (
-    <div>
-      <button>눈사람 만들 곳을 클릭해주세요!</button>
-    </div>
-  )
-}
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Wrapper } from "./HomePage";
+import Locator from "../services/locator";
 
-export default locatingPage
+
+
+const LocatingPage = () => {
+  const navigate = useNavigate();
+  
+  const goHome = () => {
+    navigate("/");
+  };
+
+  
+  
+  return (
+    <Wrapper>
+      <Locator/>
+      <Button>
+        눈사람 만들 곳을 클릭해주세요!
+        <div onClick={goHome}>x</div>
+      </Button>
+      
+    </Wrapper>
+  );
+};
+
+export default LocatingPage;
+
+const Button = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  border-radius: 100px;
+  background-color: white;
+  padding: 3%;
+  gap : 10px;
+    font-family: 'MaplestoryOTFBold';
+    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/MaplestoryOTFBold.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+`;
+
