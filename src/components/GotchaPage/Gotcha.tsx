6fb5889bfa5de4a7fdc01d8560e7bce9d276b479
jungsunbeen <tjqls041103@gmail.com>
2024-11-13 22:40:47 +0900

    FIX : 폴더정리, ADD : Gotcha

---
+++ src/components/GotchaPage/Gotcha.tsx
@@ -0,0 +1,7 @@
+const Gotcha = () => {
+  return (
+    <button>과잠 가챠 돌리기</button>
+  )
+}
+
+export default Gotcha
--- src/pages/GotchaPage.tsx
+++ src/pages/GotchaPage.tsx
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+import image from "../../public/images/gotchas/gotcha.png";
+import { Wrapper } from "./HomePage";
+import Header from "../components/Header";
+import Gotcha from "../components/GotchaPage/Gotcha";
+
 const GotchaPage = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <Wrapper>
+        <Header></Header>
+        <Image src={image} alt="gotcha" />
+        <Gotcha/>
+    </Wrapper>
+  );
 }
 
-export default GotchaPage
+export default GotchaPage;
+
+const Image = styled.img`
+    width : 100%;
+    height : 90%;
+`
