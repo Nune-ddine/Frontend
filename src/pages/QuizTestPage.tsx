f1a71767d84998c9846c4b039fae53e13847e53d
jungsunbeen <tjqls041103@gmail.com>
2024-11-17 19:59:48 +0900

    TEST

--- src/pages/QuizTestPage.tsx
+++ src/pages/QuizTestPage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import Quiz from '../components/HomePage/Quiz';
 import QuizModal from '../components/Quiz';
+import { createSnowman } from '../services/api/sunbeenTESTAPI';
+
 
 const QuizTestPage = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -12,11 +13,17 @@ const QuizTestPage = () => {
     const closeModal = () => {
       setIsModalOpen(false);
     };
+
+    const createNun = async() => {
+      const response = await createSnowman();
+      console.log(response);
+    }
   
     return (
       <div>
-        <button onClick={openModal}>퀴즈 시작하기</button>
-        <QuizModal isOpen={isModalOpen} onClose={closeModal} />
+        <button onClick={createNun}>눈사람 만들기</button>
+        {/* <button onClick={openModal}>퀴즈 시작하기</button> */}
+        {/* <QuizModal isOpen={isModalOpen} onClose={closeModal} /> */}
       </div>
     );
 }
