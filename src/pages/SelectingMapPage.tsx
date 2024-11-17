307328e678f773fe3bce786c5d88f6b4c337d5d7
jungsunbeen <tjqls041103@gmail.com>
2024-11-17 15:38:22 +0900

    DESIGN : 지도에서도 배경 적용

--- src/pages/SelectingMapPage.tsx
+++ src/pages/SelectingMapPage.tsx
@@ -5,10 +5,10 @@ import styled from "styled-components";
 import BackBtn from "../components/BackBtn";
 
 const SelectingMapPage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
-    <Wrapper>
+    <StyledWrapper>
         <Header/>
         <BackBtn/>
         <Map onClick={()=> navigate('/1')} src="/images/homes/map.png" style={{ top: "20%", left: "10%" }}/>
@@ -16,11 +16,17 @@ const SelectingMapPage = () => {
         <Map onClick={()=> navigate('/3')} src="/images/homes/map.png" style={{ top: "40%", left: "10%" }}/>
         <Map onClick={()=> navigate('/4')} src="/images/homes/map.png" style={{ top: "50%", right: "10%" }}/>
         <Map onClick={()=> navigate('/5')} src="/images/homes/map.png" style={{ top: "60%", left: "10%" }}/>
-    </Wrapper>
+    </StyledWrapper>
   )
 }
 
 export default SelectingMapPage;
+
+const StyledWrapper = styled(Wrapper)`
+  background-image: url("/images/homes/backgrounds/2.png");
+  background-size: cover;
+  background-position: center;
+`;
 
 const Map = styled.img`
 position: absolute;
