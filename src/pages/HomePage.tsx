f773ea015eaea1100be6d5657379dc39ab1e7677
suzzang2 <subin030102@naver.com>
2024-11-19 03:43:55 +0900

    FEAT: reload 취소

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -23,9 +23,7 @@ const HomePage = () => {
     if (code && !localStorage.getItem("token")) {
       login(code); // 실제 로그인 함수 호출
       // 로그인 후 화면 새로고침
-    } else {
-      window.location.reload();
-    }
+    } 
     if (location.pathname === "/") {
       navigate("/1", { replace: true });
     } 
