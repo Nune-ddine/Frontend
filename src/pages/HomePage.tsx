2d7f42472f450fa2a92e67419e1711dacae9cb6e
suzzang2 <subin030102@naver.com>
2024-11-18 20:13:26 +0900

    DESIGN: 카카오 버튼 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -37,9 +37,10 @@ const HomePage = () => {
           </MainLayout>
         ) : (
         <LoginLayout>
-          <div onClick={() => handleLoginClick()}>
+          <KakaoBtn onClick={() => handleLoginClick()}>
+            {/* 카카오 로그인하기  */}
             <img src='images/homes/kakaoLogin.png' alt="카카오 로그인하기" style={{ marginRight: '8px' , width :"90%"}} />
-          </div>
+          </KakaoBtn>
         </LoginLayout>
         )}
       {/* <button onClick={() => navigate('/making')}>Making Page</button>
@@ -96,206 +97,16 @@ export const LoginLayout = styled.div`
   }
 `;
 
-  // getMemberTest
-  // header에 토큰을 담아서 보내기
-  // const getMember = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.get("https://nuneddine.p-e.kr/api/v1/member", {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to get member test", error);
-  //   }
-  // };
-
-  // const getGotcha = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.get("https://nuneddine.p-e.kr/api/v1/item/gotcha", {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to get gotcha test", error);
-  //   }
-  // }
-
-  // // patchMemberTest
-  // // header에 토큰을 담아서 보내기
-  // const patchMember = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.patch("https://nuneddine.p-e.kr/api/v1/member/username", {
-  //       username: "테스트 닉네임",
-  //     }, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to patch member test", error);
-  //   }
-  // };
-
-  // const getInventory = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.get("https://nuneddine.p-e.kr/api/v1/item/inventory", {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to get inventory test", error);
-  //   }
-  // };
-
-  // const getAllSnowman = async () => {
-  //   try {
-  //     const response = await axios.get("https://nuneddine.p-e.kr/api/v1/map/1");
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to fetch all snowman data", error);
-  //   }
-  // };
-
-  // const getSnowmanQuiz = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.get("https://nuneddine.p-e.kr/api/v1/snowman/1", {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to get snowman quiz test", error);
-  //   }
-  // };
-
-  // const createSnowman = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.post("https://nuneddine.p-e.kr/api/v1/map/1/snowman", {
-  //       name: "수쨩테스트",
-  //       image: "이미지링크",
-  //       posX: 2,
-  //       posY: 3,
-  //       quiz: "퀴즈",
-  //       answerId: 3,
-  //       content1: "보기1",
-  //       content2: "보기2",
-  //       content3: "보기3"
-  //     }, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  //     console.log(response.data); 
-  //   } catch (error) {
-  //     console.error("Failed to create snowman", error);
-  //   }
-  // };
-
-  // const getMySnowmans = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.get("https://nuneddine.p-e.kr/api/v1/my-snowman", {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to get my snowmans", error);
-  //   }
-  // }
-
-  // const getSnowmanInfo = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.get("https://nuneddine.p-e.kr/api/v1/snowman/detail/1", {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to get snowman info", error);
-  //   }
-  // }
-
-  // const trySnowmanQuiz = async () => {
-  //   const token = localStorage.getItem("token");
-  //   if (!token) {
-  //     console.log("Token not found");
-  //     return;
-  //   }
-  
-  //   try {
-  //     const response = await axios.post("https://nuneddine.p-e.kr/api/v1/snowman/2", {
-  //       number: 1
-  //     }, {
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //       },
-  //     });
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.error("Failed to try snowman quiz", error);
-  //   }
-  // }
-
+const KakaoBtn = styled.div`
+  /* display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 60%;
+  cursor: pointer;
+  border: 1.8px solid #3b3b3bfd;
+  border-radius: 10px;
+  color: white;
+  font-size: 1.2rem;
+  background-color: #3b3b3bfd; */
+`;
